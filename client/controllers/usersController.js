1b71aeb6c2681a55a36f7558ca1e68659312b5cc
Alex Mitchell <alexj.mitchell94@gmail.com>
2020-07-05 01:49:16 -0700

    creating authentication with jsonwebtoken and bcrypt, pretty tired will need clean up and checks

--- client/controllers/usersController.js
+++ client/controllers/usersController.js
@@ -1,5 +1,8 @@
 import { Template } from "meteor/templating";
 import { Users } from "../../database/collections";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+import { HTTP } from "meteor/http";
 import SimpleSchema from "simpl-schema";
 
 const UsersController = () => {
@@ -18,7 +21,7 @@ const UsersController = () => {
           const target = event.target;
           const email = target.email.value.trim();
           const username = target.username.value.trim();
-          const password = target.password.value.trim();
+          let password = target.password.value.trim();
 
           const validationContext = Users.simpleSchema().newContext();
 
@@ -27,11 +30,47 @@ const UsersController = () => {
           validationContext.validate({ email, username, password });
 
           if (validationContext.isValid()) {
-            Users.insert({
-              email: email,
-              username: username,
-              password: password,
-            });
+            const registerUser = async () => {
+              let user = Users.findOne({ username: username });
+              if (user) {
+                console.log("user already in system");
+                return;
+              }
+
+              const salt = await bcrypt.genSalt(10);
+              password = await bcrypt.hash(password, salt);
+
+              user = Users.insert({
+                email: email,
+                username: username,
+                password: password,
+              });
+
+              const payload = {
+                user: {
+                  id: user.id,
+                },
+              };
+
+              jwt.sign(
+                payload,
+                "randomString",
+                {
+                  expiresIn: 10000,
+                },
+                (err, token) => {
+                  if (err) throw err;
+                  HTTP.call("POST", "http://localhost:3000", {
+                    data: {
+                      token: token,
+                    },
+                    statusCode: 200,
+                  });
+                }
+              );
+            };
+
+            registerUser();
 
             target.email.value = "";
             target.username.value = "";
@@ -42,6 +81,14 @@ const UsersController = () => {
             errorMessages.forEach((errors) => {
               errors.forEach((message) => {
                 console.log(message);
+                switch (message.type) {
+                  case "minString":
+                    alert(`${message.name} needs to be at least 8 characters`);
+                    break;
+                  default:
+                    console.log("something went wrong");
+                    break;
+                }
               });
             });
           }
